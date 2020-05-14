96557a77f78608e6d2981c61beb96412fc7cfbad
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-14 18:23:00 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,5 +1,9 @@
 def count_strings(array)
-  array.count do
+  # Return the total number of strings in the provided array using the count enumerable
+end
+
+def count_strings(array)
+  array.count do |string|
     array.class() == String
   end
   
